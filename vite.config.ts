7ca7b2c3904b5d5e67a533196a70897905f1171e
Alexandros_Liaskos <alexliaskosga@gmail.com>
2024-11-28 23:29:25 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Web_MS/',
+  base: '/Web_Launcher/',
   server: {
     port: 3001,
     open: true
